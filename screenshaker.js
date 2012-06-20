3911d87c74c1a79fc9f793e1e73c41b68eefb828
killbot <davidhoman314@gmail.com>
2012-06-20 10:13:36 -0500

    adding license to screenshaker.js

--- screenshaker.js
+++ screenshaker.js
@@ -2,7 +2,20 @@
 //impact 1.19 and 1.20.  Will probably work in other versions, but
 //i haven't tested those.
 /*
+           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                   Version 2, December 2004
 
+Copyright (C) 2012 Dave Homan <owlbear@slouchcou.ch>
+Created during production of Drugbound <http://drugbound.com>
+
+Everyone is permitted to copy and distribute verbatim or modified
+copies of this license document, and changing it is allowed as long
+as the name is changed.
+
+           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+ 0. You just DO WHAT THE FUCK YOU WANT TO.
 */
 
 
